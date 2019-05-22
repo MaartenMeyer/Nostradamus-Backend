ba6ade2fe39cf4304ee9ff795f2c5034bf1c280f
Mick-Theuns <mtheuns1@avans.nl>
2019-05-22 12:40:26 +0200

    sql connection

--- src/datalayer/mysql.dao.js
+++ src/datalayer/mysql.dao.js
@@ -1,34 +1,25 @@
-const sql     = require('mysql');
+const mysql     = require('mysql');
 const config  = require('../config/appconfig');
+const logger = require("tracer").colorConsole();
 
-const logger    = config.logger;
-const dbconfig  = config.dbconfig;
+// If online database doesnt work these are the settings for the online database
+let database = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  databasename: 'nostradamus',
+  insecureAuth: true
+})
 
-module.exports = {
-  executeQuery: (query, callback) => {
-    sql.connect(dbconfig, err => {
 
-      if (err) {
-        logger.error('Error connecting: ', err.toString());
-        callback(err, null);
-        sql.close()
-      }
-
-      if (!err) {
-        new sql.Request().query(query, (err, result) => {
-
-          if (err) {
-            logger.error('error', err.toString());
-            callback(err, null);
-            sql.close()
-          }
-
-          if (result) {
-            callback(null, result);
-            sql.close()
-          }
-        })
-      }
-    })
+database.connect( (error) => {
+  console.log('connectiong pending...')
+  if (error){
+    console.log(error)
+    return
+  } else {
+    console.log('Connected with database')
   }
-};
+})
+
+module.exports = database
