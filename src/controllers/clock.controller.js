61d2ee9c21dc1429f3bbcecbda952fff3290cccd
Mick-Theuns <mtheuns1@avans.nl>
2019-05-23 12:32:24 +0200

    add clockroutes and handler

--- src/controllers/clock.controller.js
+++ src/controllers/clock.controller.js
@@ -5,9 +5,15 @@ const assert        = require("assert");
 module.exports = {
     clockin: (req, res, next)=>{
         logger.info("Clocking in was called")
+
     },
 
     clockoff: (req,res,next)=>{
         logger.info("Clocking off was called")
+
+    },
+
+    clockHandler: (req,res,next)=>{
+        logger.info("Clockinghandler was called")
     }
 };
--- src/routes/clock.routes.js
+++ src/routes/clock.routes.js
@@ -1,9 +1,11 @@
 const express                   = require("express");
 const router                    = express.Router();
 const clockController           = require("../controllers/clock.controller");
+const authenticationController  = require("../controllers/authentication.controller");
 
 
-router.post("/clockin", clockController.clockin);
-router.post("/clockoff", clockController.clockoff);
+
+router.post("/clocking", clockController.clockHandler);
+// router.post("/clock\", clockController.clockoff);
 
 module.exports = router;
