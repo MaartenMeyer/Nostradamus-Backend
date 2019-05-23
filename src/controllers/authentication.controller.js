b5fcbbcc196da1aa8ad3f83369d3342330834273
MouaouiaB <m-maroc-1998@hotmail.com>
2019-05-23 11:35:07 +0200

    update som regex

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -8,7 +8,7 @@ const bcrypt        = require("bcryptjs");
 saltRounds = 10;
 
 // Regex voor check
-const emailValidator      = new RegExp('^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
+  const emailValidator      = new RegExp('^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
 const dateValidator       = new RegExp('([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))');
 const passwordValidator   = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$');
 
@@ -26,7 +26,7 @@ module.exports = {
       assert.equal(typeof user.lastName, "string", "A valid lastName is required.");
       assert(dateValidator.test(user.dateOfBirth), "A valid dateOfBirth is required.");
       assert(emailValidator.test(user.emailAddress), "A valid mailAddress is required.");
-      assert(passwordValidator.test(user.password), "A valid password is required.");
+      //assert(passwordValidator.test(user.password), "A valid password is required.");
       assert.equal(typeof user.accountType,  "number","A valid accountType is required.");
       assert.equal(typeof user.userNumber, "number", "A valid userNumber is required");
 
@@ -35,9 +35,9 @@ module.exports = {
       const query =
           "INSERT INTO `nostradamus`.`user` (`firstName`, `lastName`, `dateOfBirth`, `emailAddress`, `password`, `accountType`, `userNumber`) " +
           "VALUES ('" + user.firstName + "', '" + user.lastName + "', '" + user.dateOfBirth + "', '" + user.emailAddress + "', '" + hash + "','" + user.accountType + "', '" + user.userNumber + "')"
-          " LAST_INSERT_ID();";
+      " LAST_INSERT_ID();";
 
-        logger.info(query);
+      logger.info(query);
 
       database.query(query, (err, rows) => {
         if (err) {
