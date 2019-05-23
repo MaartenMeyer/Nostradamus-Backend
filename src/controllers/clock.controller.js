739993d89f17385f77a6e7a57bfa6ff8056583db
MouaouiaB <m-maroc-1998@hotmail.com>
2019-05-23 12:31:14 +0200

    add clokcin funciton to clock.controller

--- src/controllers/clock.controller.js
+++ src/controllers/clock.controller.js
@@ -5,6 +5,29 @@ const assert        = require("assert");
 module.exports = {
     clockin: (req, res, next)=>{
         logger.info("Clocking in was called")
+
+        // hier komt in het request een binnen.
+        const clock = req.body
+        logger.info(clock)
+
+        const query =
+            "INSERT INTO nostradamus.clocking_system(userNumber, beginTime, branchId, departmentId) " +
+            "VALUES ('" + clock.userNumber + "','now()','" +
+            clock.branchId + "','" + clock.department + + "');";
+
+        database.query(query, (err, rows) => {
+            // verwerk error of result
+            if (err) {
+                const errorObject = {
+                    message: 'Something went wrong in the database.',
+                    code: 500
+                }
+                next(errorObject)
+            }
+            if (rows) {
+                res.status(200).json({ result: rows.recordset})
+            }
+        })
     },
 
     clockoff: (req,res,next)=>{
