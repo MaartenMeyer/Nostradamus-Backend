2bcd6a282d3b83337a778f86a65fd58433edc572
Jasper van Vliet <jvliet6@avans.nl>
2019-05-29 10:46:05 +0200

    Revert "Revert "no message""
    This reverts commit dad9d4fa2be32a67cc8745a0b4ee24cf0202a2c2.

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 // The used libraries from node_modules.
 const chai      = require('chai');
 const chaiHttp  = require('chai-http');
-const jwt       = require('jsonwebtoken');
 const server    = require('./app.js');
 
 chai.should();
@@ -9,23 +8,6 @@ chai.use(chaiHttp);
 
 // The connection with the authorization.
 const authorization = 'Authorization';
-
-let token;
-
-//Information needed for the tests
-before(() => {
-  console.log('before');
-
-  const payload = {
-    UserId: 6
-  };
-
-  jwt.sign({ data: payload }, 'secretkey', { expiresIn: 60 * 60 * 24 }, (err, result) => {
-    if (result) {
-      token = result;
-    }
-  })
-});
 
 // The tests for registration.
 describe('Register', () => {
@@ -37,12 +19,12 @@ describe('Register', () => {
       .send({
         "firstName": "Rick",
         "lastName": "van Vliet",
-        "userName": "rvvliet13",
+        "userName": "rvvliet17",
         "dateOfBirth": "1999-08-24",
-        "emailAddress": "helloworld13@gmail.com",
+        "emailAddress": "helloworld17@gmail.com",
         "password": "HelloWorld66",
         "accountType": 1,
-        "userNumber": 12566613
+        "userNumber": 12566617
       })
 
       .end(function (err, res) {
@@ -51,7 +33,6 @@ describe('Register', () => {
         done()
       })
   });
-
   it('Register a not valid user.', done => {
     chai.request(server)
       .post('/api/register')
@@ -84,7 +65,7 @@ describe('Login', () => {
       .set('Content-Type', 'application/json')
 
       .send( {
-        "userName": "rvvliet12",
+        "userName": "rvvliet14",
         "password": "HelloWorld66"
       })
 
@@ -94,7 +75,6 @@ describe('Login', () => {
         done()
       })
   });
-
   it('Login with a not valid user.', done => {
     chai.request(server)
       .post('/api/login')
@@ -115,13 +95,13 @@ describe('Login', () => {
 
 // The tests for clocking.
 describe('Clocking', () => {
-  it('Clocking with a valid userNumber.', done => {
+  it('Clocking with a valid userNumber and a valid token.', done => {
     chai.request(server)
       .post('/api/clocking')
-      .set(authorization, 'Bearer ' + token)
+      .set(authorization, 'Bearer ' + "'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IlVzZXJJZCI6M30sImlhdCI6MTU1OTA0MzA4NiwiZXhwIjoxNTU5MTI5NDg2fQ.OQfwa04KniWj0RURTLnU5ITpKjRrDKZVj8QqBykvYHc'")
 
         .send( {
-        "userNumber": 1256663,
+        "userNumber": 12345,
         "branchId": 1,
         "departmentId": 1
       })
@@ -131,33 +111,31 @@ describe('Clocking', () => {
         done()
       })
   });
-
-  it('Clocking with a not valid userNumber.', done => {
+  it('Clocking with a not valid userNumber, but a valid token', done => {
     chai.request(server)
       .post('/api/clocking')
-      .set(authorization, 'Bearer ' + token)
+      .set(authorization, 'Bearer ' + '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IlVzZXJJZCI6M30sImlhdCI6MTU1OTA0MzA4NiwiZXhwIjoxNTU5MTI5NDg2fQ.OQfwa04KniWj0RURTLnU5ITpKjRrDKZVj8QqBykvYHc"')
 
         .send( {
-        "userNumber": 0,
+        "userNumber": 637392727378732,
         "branchId": 1,
         "departmentId": 1
       })
 
       .end(function (err, res) {
-        res.should.have.status(401);
+        res.should.have.status(500);
         res.body.should.be.a('object');
         done()
       })
   });
-
-  it('clocking with a not valid token', done => {
+  it('Clocking with a valid userNumber, but a not valid token', done => {
     chai.request(server)
       .post('/api/clocking')
       .set(authorization, 'Bearer ' + '"sdfdxdbxbgsskj-/sfvxdfsbdffghsdfb"')
 
         .send({
-        "userNumber": 14,
-        "branchId": 2,
+        "userNumber": 12345,
+        "branchId": 1,
         "departmentId": 1
       })
 
@@ -167,5 +145,80 @@ describe('Clocking', () => {
         done();
       })
   })
+});
+
+// The tests for breaking.
+describe('Breaking', () => {
+    it('Breaking with a valid userNumber and a valid token.', done => {
+       chai.request(server)
+           .post('/api/breaking')
+           .set(authorization, 'Bearer ' + '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IlVzZXJJZCI6M30sImlhdCI6MTU1OTA0MzA4NiwiZXhwIjoxNTU5MTI5NDg2fQ.OQfwa04KniWj0RURTLnU5ITpKjRrDKZVj8QqBykvYHc"')
+
+           .send( {
+               "userNumber": 12345
+           })
+
+           .end(function (err, res) {
+               res.should.have.status(200);
+               res.body.should.be.a('object');
+               done();
+           })
+   });
+    it('Breaking with a not valid userNumber, but a valid token.', done => {
+        chai.request(server)
+            .post('/api/breaking')
+            .set(authorization, 'Bearer ' + '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IlVzZXJJZCI6M30sImlhdCI6MTU1OTA0MzA4NiwiZXhwIjoxNTU5MTI5NDg2fQ.OQfwa04KniWj0RURTLnU5ITpKjRrDKZVj8QqBykvYHc"')
+
+            .send( {
+                "userNumber": 535265736782
+            })
+
+            .end(function (err, res) {
+                res.should.have.status(500);
+                res.body.should.be.a('object');
+                done();
+            })
+    });
+    it('Breaking with a valid userNumber, but a not valid token.', done => {
+        chai.request(server)
+            .post('/api/breaking')
+            .set(authorization, 'Bearer ' + '"5MTI4MTk1fQ.xa5zclkUvlOjqYPaBOHTvJ9ekvJBdyoN5ZuNOS2NwN8"')
+
+            .send( {
+                "userNumber": 12345
+            })
+
+            .end(function (err, res) {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+                done();
+            })
+    })
+});
+
+// The tests for active.
+describe('Active', () => {
+   it('Get active employees by a valid token.', done => {
+      chai.request(server)
+          .get('/api/active')
+          .set(authorization, 'Bearer ' + "'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IlVzZXJJZCI6M30sImlhdCI6MTU1OTA0MzA4NiwiZXhwIjoxNTU5MTI5NDg2fQ.OQfwa04KniWj0RURTLnU5ITpKjRrDKZVj8QqBykvYHc'")
+
+          .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              done();
+          })
+   });
+    it('Get active employees by a not valid token.', done => {
+        chai.request(server)
+            .get('/api/active')
+            .set(authorization, 'Bearer ' + '"degryu4y43y72748842y4hdu4hyd4y4387"')
+
+            .end(function (err, res) {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+                done();
+            })
+    })
 });
 
