4d7710b357419f4eb3746135b97a2efad3c49105
Maarten <maartenmeyer8@gmail.com>
2019-05-31 14:38:58 +0200

    Fixed json formatting

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -8,7 +8,7 @@ module.exports = {
         logger.info('getDataOfUser is called')
         const id = req.params.userId;
 
-        const query = `SELECT b.branchId, b.branchName, d.departmentId, d.departmentName FROM nostradamus.department d
+        const query = `SELECT c.companyId, b.branchId, b.branchName, d.departmentId, d.departmentName FROM nostradamus.department d
                             INNER JOIN nostradamus.branch_department bd ON d.departmentId = bd.departmentId
                             INNER JOIN nostradamus.branch b ON bd.branchId = b.branchId
                             INNER JOIN nostradamus.company_branch cb ON b.branchId = cb.branchId
@@ -30,7 +30,60 @@ module.exports = {
             }
             if (rows) {
                 if (rows.length > 0) {
-                    res.status(200).json(rows)
+
+                    function contains(arr, id) {
+                        for (var i = 0; i < arr.length; i++) {
+                            if (arr[i].branchId === id) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                    function containsDepartment(arr, id) {
+                        for (var i = 0; i < arr.length; i++) {
+                            if (arr[i].departmentId === id) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+
+                    var company = {
+                        companyId: "",
+                        branches: []
+                    };
+                    company.companyId = rows[0].companyId;
+
+                    for(i = 0; i < rows.length; i++){
+                        var branch = {
+                            branchId: "",
+                            branchName: "",
+                            departments: []
+                        };
+                        branch.branchId = rows[i].branchId;
+                        branch.branchName = rows[i].branchName;
+
+                        if(!contains(company.branches, branch.branchId)){
+                            for(j = 0; j < rows.length; j++){
+                                var department = {
+                                    departmentId: "",
+                                    departmentName: ""
+                                };
+                                department.departmentId = rows[j].departmentId;
+                                department.departmentName = rows[j].departmentName;
+
+                                if(rows[j].branchId == branch.branchId){
+                                    if(!containsDepartment(branch.departments, department.departmentId)){
+                                        branch.departments.push(department);
+                                    }
+                                }
+                            }
+                            branch.departments.sort((a, b) => (a.departmentId > b.departmentId) ? 1 : -1);
+
+                            company.branches.push(branch);
+                        }
+                    }
+                    res.status(200).json(company)
                 } else {
                     const errorObject = {
                         message: 'UserId not found.',
