beaa213927d63cbe585978157d948e12ae371d1d
Maarten <maartenmeyer8@gmail.com>
2019-06-01 15:00:53 +0200

    Documentation of data.controller.js

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -3,7 +3,7 @@ const logger = require("../config/appconfig").logger;
 const database = require("../datalayer/mysql.dao");
 
 module.exports = {
-    // Function to get all departments of the company of the given user id
+    // Function to get data of the company of the given user id in parsed JSON format
     getDataOfUser: (req, res, next) => {
         logger.info('getDataOfUser is called')
         const id = req.params.userId;
@@ -31,16 +31,28 @@ module.exports = {
             if (rows) {
                 if (rows.length > 0) {
 
+                    /**
+                     *
+                     * @param {Array} arr branches
+                     * @param {number|string} id branchId
+                     * @return {boolean} Whether param arr contained param id
+                     */
                     function contains(arr, id) {
-                        for (var i = 0; i < arr.length; i++) {
+                        for (let i = 0; i < arr.length; i++) {
                             if (arr[i].branchId === id) {
                                 return true;
                             }
                         }
                         return false;
                     }
+                    /**
+                     *
+                     * @param {Array} arr departments
+                     * @param {number|string} id departmentId
+                     * @return {boolean} Whether param arr contained param id
+                     */
                     function containsDepartment(arr, id) {
-                        for (var i = 0; i < arr.length; i++) {
+                        for (let i = 0; i < arr.length; i++) {
                             if (arr[i].departmentId === id) {
                                 return true;
                             }
@@ -48,38 +60,51 @@ module.exports = {
                         return false;
                     }
 
-                    var company = {
+                    // Declaration of object company with companyId and Array named branches
+                    let company = {
                         companyId: "",
                         branches: []
                     };
+                    // Sets companyId of company to the companyId of the first element of Array rows
                     company.companyId = rows[0].companyId;
 
-                    for(i = 0; i < rows.length; i++){
-                        var branch = {
+                    // Loops through all elements of Array rows
+                    for(let i = 0; i < rows.length; i++){
+                        // Declaration of object branch with branchId, branchName and Array named departments
+                        let branch = {
                             branchId: "",
                             branchName: "",
                             departments: []
                         };
+                        // Sets branchId and branchName to the branchId and branchName of element i of Array rows
                         branch.branchId = rows[i].branchId;
                         branch.branchName = rows[i].branchName;
 
+                        // Checks if Array company.branches already contains the branch
+                        // If not, adds departments to Array departments in branch, and finally adds branch to company.branches
                         if(!contains(company.branches, branch.branchId)){
-                            for(j = 0; j < rows.length; j++){
-                                var department = {
+                            for(let j = 0; j < rows.length; j++){
+                                // Declaration of object department with departmentId and departmentName
+                                let department = {
                                     departmentId: "",
                                     departmentName: ""
                                 };
+                                // Sets departmentId and departmentName to the departmentId and departmentName of element j of Array rows
                                 department.departmentId = rows[j].departmentId;
                                 department.departmentName = rows[j].departmentName;
 
                                 if(rows[j].branchId == branch.branchId){
+                                    // Checks if Array branch.departments already contains the department
+                                    // If not, adds department to Array departments in branch
                                     if(!containsDepartment(branch.departments, department.departmentId)){
                                         branch.departments.push(department);
                                     }
                                 }
                             }
+                            // Sorts departments in branch.departments ascending based on departmentId
                             branch.departments.sort((a, b) => (a.departmentId > b.departmentId) ? 1 : -1);
 
+                            // Adds branch to company.branches
                             company.branches.push(branch);
                         }
                     }
