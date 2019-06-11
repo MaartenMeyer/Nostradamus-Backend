cbde7ec5e75fbb60492cb8fcb5393cacdfb69bb6
Maarten <maartenmeyer8@gmail.com>
2019-06-11 20:29:31 +0200

    Revert "No big changes, just cleaning the code a bit"
    This reverts commit c2baaa4461d55fa4955f6ebf77830ea14826849a.

--- src/controllers/clock.controller.js
+++ src/controllers/clock.controller.js
@@ -3,17 +3,21 @@ const logger        = require("../config/appconfig").logger;
 const database      = require("../datalayer/mysql.dao");
 
 module.exports = {
-    clockHandler: (req,res,next) => {
+  clockHandler: (req,res,next)=>{
         logger.info("clockHandler was called.");
         const user = req.body;
 
-        // If there is still a pause clocked in, it is now clocked out.
+        // If there is still a pause clocked in, it is now clocked out
         const query2 = "SELECT 1 FROM nostradamus.break_system WHERE endTime IS NULL AND userNumber = " + user.userNumber + ";";
 
         database.query(query2, (err, rows)=>{
             if (rows.length > 0){
+
                 let query = "";
 
+                // Checks if request body contains startTime and endTime
+                // If only endtime given: set endTime of break to the same endTime
+                // Else: updates break to set endTime to now()
                 if(user.startTime == null && user.endTime != null){
                     query = "UPDATE `nostradamus`.`break_system` SET `endTime` = '" + user.endTime + "' WHERE userNumber = " + user.userNumber + " AND endTime IS NULL;";
                 } else {
@@ -21,29 +25,34 @@ module.exports = {
                 }
 
                 database.query(query, (err, rows)=>{
-                logger.info("Users break is clocked off.");
+                logger.info("USER ALSO BREAK CLOCKED OFF");
                 });
             }
         });
 
         // select 1 is for faster query searching
         const clock = req.body;
+
         const query = "SELECT 1 FROM nostradamus.clocking_system WHERE userNumber = " + user.userNumber + " AND endTime IS NULL;";
 
         // Return error or result.
         database.query(query, (err, rows) => {
             if (err) {
                 const errorObject = {
-                    message: 'Error in database at SELECT 1 FROM nostradamus.clocking_system.',
+                    message: 'Error in database at SELECT 1 FROM nostradamus.clocking_system',
                     code: 500
                 };
                 next(errorObject);
             }
 
             if (rows.length > 0) {
+
                 let query = "";
 
-                 if (user.startTime == null && user.endTime != null) {
+                // Checks if request body contains startTime and endTime
+                // If only endtime given: set endTime of clocking entry to the same endTime
+                // Else: update clocking record to set endTime to now()
+                if (user.startTime == null && user.endTime != null) {
                     query = "UPDATE `nostradamus`.`clocking_system` SET `endTime` = '" + user.endTime + "' WHERE (endTime IS null AND userNumber = " + clock.userNumber + ");";
                 } else {
                     query = "UPDATE `nostradamus`.`clocking_system` SET `endTime` = now() WHERE (endTime IS null AND userNumber = " + clock.userNumber + ");";
@@ -53,7 +62,7 @@ module.exports = {
                 database.query(query, (err, rows) => {
                     if (err) {
                         const errorObject = {
-                            message: 'Error in database at UPDATE nostradamus.clocking_system.',
+                            message: 'Error in database at UPDATE nostradamus.clocking_system',
                             code: 500
                         };
                         next(errorObject);
@@ -63,20 +72,19 @@ module.exports = {
                         res.status(200).json({ message: 'User is clocked off.' });
                     }
                 });
-            }
+            }else {
 
-            else {
                 let query = "";
 
+                // Checks if request body contains startTime and endTime
+                // If both startTime and endTime given: insert new record with given startTime and endTime
+                // If only startTime given: insert new record with given startTime
+                // Else: insert new record with startTime set to now()
                 if (user.startTime != null && user.endTime != null) {
                     query = "INSERT INTO nostradamus.clocking_system(userNumber, beginTime, endTime, branchId, departmentId) VALUES ('" + user.userNumber + "','" + user.startTime + "','" + user.endTime +"','" + user.branchId + "','" + user.departmentId + "')";
-                }
-
-                else if (user.startTime != null && user.endTime == null) {
+                } else if (user.startTime != null && user.endTime == null) {
                     query = "INSERT INTO nostradamus.clocking_system(userNumber, beginTime, branchId, departmentId) VALUES ('" + user.userNumber + "','" + user.startTime + "','" + user.branchId + "','" + user.departmentId + "')";
-                }
-
-                else {
+                } else {
                     query = "INSERT INTO nostradamus.clocking_system(userNumber, beginTime, branchId, departmentId) VALUES ('" + clock.userNumber + "',now(),'" + clock.branchId + "','" + clock.departmentId + "')";
                 }
 
@@ -84,7 +92,7 @@ module.exports = {
                 database.query(query, (err, rows) => {
                     if (err) {
                         const errorObject = {
-                            message: 'Error in database at INSERT INTO nostradamus.clocking_system.',
+                            message: 'Error in database at INSERT INTO nostradamus.clocking_system',
                             code: 500
                         };
                         next(errorObject);
@@ -98,6 +106,9 @@ module.exports = {
         });
     },
 
+    // Returns clocking_system object if a row is found in database with the given userNumber and with endTime NULL
+    // Else: returns 404 status
+    // If userNumber is not found, returns 500 status
     clockStatus: (req, res, next) =>{
         logger.info("clockStatus was called.");
         const userNumber = req.params.userNumber;
@@ -107,13 +118,14 @@ module.exports = {
         database.query(query, (err, rows) => {
             if (err) {
                 const errorObject = {
-                    message: 'Error in database at SELECT 1 FROM nostradamus.clocking_system.',
+                    message: 'Error in database at SELECT 1 FROM nostradamus.clocking_system',
                     code: 500
                 };
                 next(errorObject);
             }
 
             if (rows.length > 0) {
+
                 let clockingEntry = {
                     clockingSystemId: "",
                     userNumber: "",
@@ -122,17 +134,15 @@ module.exports = {
                     branchId: "",
                     departmentId: ""
                 };
-
                 clockingEntry.clockingSystemId = rows[0].clockingSystemId;
                 clockingEntry.userNumber = rows[0].userNumber;
                 clockingEntry.beginTime = rows[0].beginTime;
                 clockingEntry.endTime = rows[0].endTime;
                 clockingEntry.branchId = rows[0].branchId;
                 clockingEntry.departmentId = rows[0].departmentId;
-                res.status(200).json(clockingEntry);
-            }
 
-            else {
+                res.status(200).json(clockingEntry);
+            } else {
                 res.status(404).json({ message: 'No clocking entries found with endTime NULL.' });
             }
         });
@@ -191,7 +201,9 @@ module.exports = {
 
                 else {
                     const breaking = req.body;
-                    const query = "UPDATE `nostradamus`.`break_system` SET `endTime` = now() WHERE (`userNumber` = '" + breaking.userNumber + "' AND endTime IS NULL);";
+
+                    const query =
+                        "UPDATE `nostradamus`.`break_system` SET `endTime` = now() WHERE (`userNumber` = '" + breaking.userNumber + "' AND endTime IS NULL);";
 
                     // Return error or result
                     database.query(query, (err, rows) =>{
@@ -210,42 +222,46 @@ module.exports = {
         });
     },
 
+    // Returns breaking_system object if a row is found in database with the given userNumber and with endTime NULL
+    // Else: returns 404 status
+    // If userNumber is not found, returns 500 status
     breakStatus: (req, res, next) => {
         logger.info("breakStatus was called.");
         const userNumber = req.params.userNumber;
+
         const query = "SELECT * FROM nostradamus.break_system WHERE userNumber = " + userNumber + " AND endTime IS NULL;";
 
         database.query(query, (err, rows) => {
             if (err) {
                 const errorObject = {
-                    message: 'Error in database at SELECT 1 FROM nostradamus.break_system.',
+                    message: 'Error in database at SELECT 1 FROM nostradamus.break_system',
                     code: 500
                 };
                 next(errorObject);
             }
 
             if (rows.length > 0) {
+
                 let breakEntry = {
                     breakSystemId: "",
                     beginTime: "",
                     endTime: ""
                 };
-
 				breakEntry.breakSystemId = rows[0].break_systemId;
 				breakEntry.beginTime = rows[0].beginTime;
 				breakEntry.endTime = rows[0].endTime;
-                res.status(200).json(breakEntry);
-            }
 
-            else {
+                res.status(200).json(breakEntry);
+            } else {
                 res.status(404).json({ message: 'No break entries found with endTime NULL and userNumber ' + userNumber });
             }
         });
     },
 
-   hoursHandler: (req,res,next) => {
-    logger.info("hoursHandler is called.");
+  hoursHandeler: (req,res,next) => {
+    logger.info("hoursHandeler is called.");
     const user = req.body;
+
     const query = "SELECT userNumber, beginTime, endTime FROM nostradamus.clocking_system where userNumber = " + user.userNumber + ";";
 
     // Returns error or result.
@@ -257,10 +273,9 @@ module.exports = {
         };
         next(errorObject)
       }
-
       if (rows){
-          res.status(200).json({ result: rows})
+        res.status(200).json({ result: rows})
       }
     })
-  }
+  },
 };
