bb71e8f24daf500227065b140c96829d70775935
Maarten <maartenmeyer8@gmail.com>
2019-06-11 20:15:59 +0200

    Revert commit

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -3,9 +3,8 @@ const logger = require("../config/appconfig").logger;
 const database = require("../datalayer/mysql.dao");
 
 module.exports = {
-    // Function to get data of the company of the given user id in parsed JSON format
     getDataOfUser: (req, res, next) => {
-        logger.info('getDataOfUser is called')
+        logger.info('getDataOfUser is called');
         const id = req.params.userId;
 
         const query = `SELECT c.companyId, b.branchId, b.branchName, d.departmentId, d.departmentName FROM nostradamus.department d
@@ -18,25 +17,20 @@ module.exports = {
                                         INNER JOIN nostradamus.user_company uc ON c.companyId = uc.companyId
                                         INNER JOIN nostradamus.user u ON uc.userId = u.UserId
                                         WHERE u.UserId=${id}
-                                    );`
+                                    );`;
 
         database.query(query, (err, rows) => {
             if (err) {
                 const errorObject = {
                     message: 'Something went wrong with the database.',
                     code: 500
-                }
+                };
                 next(errorObject);
             }
+
             if (rows) {
                 if (rows.length > 0) {
 
-                    /**
-                     *
-                     * @param {Array} arr branches
-                     * @param {number|string} id branchId
-                     * @return {boolean} Whether param arr contained param id
-                     */
                     function contains(arr, id) {
                         for (let i = 0; i < arr.length; i++) {
                             if (arr[i].branchId === id) {
@@ -45,12 +39,7 @@ module.exports = {
                         }
                         return false;
                     }
-                    /**
-                     *
-                     * @param {Array} arr departments
-                     * @param {number|string} id departmentId
-                     * @return {boolean} Whether param arr contained param id
-                     */
+
                     function containsDepartment(arr, id) {
                         for (let i = 0; i < arr.length; i++) {
                             if (arr[i].departmentId === id) {
@@ -60,60 +49,53 @@ module.exports = {
                         return false;
                     }
 
-                    // Declaration of object company with companyId and Array named branches
                     let company = {
                         companyId: "",
                         branches: []
                     };
-                    // Sets companyId of company to the companyId of the first element of Array rows
+
                     company.companyId = rows[0].companyId;
 
-                    // Loops through all elements of Array rows
                     for(let i = 0; i < rows.length; i++){
-                        // Declaration of object branch with branchId, branchName and Array named departments
                         let branch = {
                             branchId: "",
                             branchName: "",
                             departments: []
                         };
-                        // Sets branchId and branchName to the branchId and branchName of element i of Array rows
+
                         branch.branchId = rows[i].branchId;
                         branch.branchName = rows[i].branchName;
 
-                        // Checks if Array company.branches already contains the branch
-                        // If not, adds departments to Array departments in branch, and finally adds branch to company.branches
                         if(!contains(company.branches, branch.branchId)){
                             for(let j = 0; j < rows.length; j++){
-                                // Declaration of object department with departmentId and departmentName
+
                                 let department = {
                                     departmentId: "",
                                     departmentName: ""
                                 };
-                                // Sets departmentId and departmentName to the departmentId and departmentName of element j of Array rows
+
                                 department.departmentId = rows[j].departmentId;
                                 department.departmentName = rows[j].departmentName;
 
-                                if(rows[j].branchId == branch.branchId){
-                                    // Checks if Array branch.departments already contains the department
-                                    // If not, adds department to Array departments in branch
+                                if(rows[j].branchId === branch.branchId){
                                     if(!containsDepartment(branch.departments, department.departmentId)){
                                         branch.departments.push(department);
                                     }
                                 }
                             }
-                            // Sorts departments in branch.departments ascending based on departmentId
-                            branch.departments.sort((a, b) => (a.departmentId > b.departmentId) ? 1 : -1);
 
-                            // Adds branch to company.branches
+                            branch.departments.sort((a, b) => (a.departmentId > b.departmentId) ? 1 : -1);
                             company.branches.push(branch);
                         }
                     }
                     res.status(200).json(company)
-                } else {
+                }
+
+                else {
                     const errorObject = {
                         message: 'Error retrieving data: check if database contains departments and branches.',
                         code: 404
-                    }
+                    };
                     next(errorObject);
                 }
             }
@@ -121,7 +103,7 @@ module.exports = {
     },
 
     getUsersOfCompany: (req, res, next) => {
-        logger.info('getUsersOfCompany is called')
+        logger.info('getUsersOfCompany is called.');
         const id = req.params.companyId;
 
         const query = `SELECT u.UserId, u.userNumber FROM nostradamus.user u
@@ -132,16 +114,17 @@ module.exports = {
                                         INNER JOIN nostradamus.user_company uc ON c.companyId = uc.companyId
                                         INNER JOIN nostradamus.user u ON uc.userId = u.UserId
                                         WHERE u.UserId=${id}
-                                    );`
+                                    );`;
 
         database.query(query, (err, rows) => {
             if (err) {
                 const errorObject = {
                     message: 'Something went wrong with the database.',
                     code: 500
-                }
+                };
                 next(errorObject);
             }
+
             if (rows) {
                 if (rows.length > 0) {
 
@@ -149,24 +132,26 @@ module.exports = {
                         users: []
                     };
 
-                    // Loops through all elements of Array rows
                     for (let i = 0; i < rows.length; i++) {
-
                         let user = {
                             userId: "",
                             userNumber: ""
                         };
+
                         user.userId = rows[i].UserId;
                         user.userNumber = rows[i].userNumber;
-
                         users.users.push(user);
                     }
+
                     res.status(200).json(users);
-                } else {
+
+                }
+
+                else {
                     const errorObject = {
                         message: 'Error retrieving data: check if database contains users.',
                         code: 404
-                    }
+                    };
                     next(errorObject);
                 }
             }
